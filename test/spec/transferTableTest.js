2e7aee94f4b5cebc9d825de93b2dc72aa06e833d
Vunb <nhubaovu@gmail.com>
2017-03-14 11:47:10 +0700

    seperate sql, mdb connection strings

--- test/spec/transferTableTest.js
+++ test/spec/transferTableTest.js
@@ -2,6 +2,7 @@ const test = require('tape');
 const dbConfig = require('./dbConfig');
 const SqlConnection = require('../../sqlcon');
 const mongoose = require('mongoose');
+
 mongoose.Promise = require('bluebird');
 mongoose.connection.on('connected', () => {
   console.log('%s MongoDB connection established!');
@@ -10,12 +11,15 @@ mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.');
   process.exit();
 });
+
 test('Transfer MSSQL to Mongodb', function (t) {
     t.plan(1);
     // init new connection
-    const sqlCon = new SqlConnection(dbConfig);
+    let sqlCon = new SqlConnection(dbConfig.sql_connection);
+    let mdbCon = dbConfig.mdb_connection;
+    let mdbConStr = `mongodb://${mdbCon.username}:${mdbCon.password}@${mdbCon.hostname}/${mdbCon.database}`;
     sqlCon.transferTable('HT_USER_GROUP', {
-        mongodb: 'mongodb://hosoytev2:ehrdev%40146@124.158.7.56:25101/hosoytedb'
+        mongodb: mdbConStr
     })
         .then(tableRowsCount => {
             console.log('tableRowsCount:', tableRowsCount);
